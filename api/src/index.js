a5a248300039f1f4d31e91f5508edad7ca37094e
Gabriel <bielmelodossantos8@gmail.com>
2021-09-10 11:50:52 -0300

    dev

---
+++ api/src/index.js
@@ -0,0 +1,43 @@
+import db from './db.js';
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/matricula', async(req,resp) => {
+    try{
+        let alunos = await
+            db.tb_matricula.findAll({
+                order:[
+                    ['id_matricula', 'desc']
+                ]
+            })
+        resp.send(alunos)
+    } catch(e){
+        resp.send({erro: e.toString()})
+    }
+})
+
+app.post('/matricula', async(req,resp) => {
+    try{
+        let { nome, chamada, curso, turma} = req.body;
+        let ins = await
+            db.tb_matricula.create({
+                nm_aluno: nome,
+                nr_chamada: chamada,
+                nm_curso: curso,
+                nm_turma: turma
+            });
+        resp.send(ins);
+    } catch(e){
+        resp.send({erro: e.toString()})
+    }
+})
+
+
+
+app.listen(process.env.PORT,
+
+    x => console.log(`Server up at port ${process.env.PORT}`))
